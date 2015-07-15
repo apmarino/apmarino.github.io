855c5ec76b56dcb47e92da131af27b295ee8504f
Alexander Marino <marinoalexp@gmail.com>
2015-07-15 16:35:39 -0400

    imgUrl key to newDecks

--- blackjack_script.js
+++ blackjack_script.js
@@ -9,261 +9,313 @@ var newDeck ={
     "value": 2,
     "name": "2",
     "suit": "spade",
+    "imgUrl": ""
   },
   "1": {
     "value": 3,
     "name": "3",
     "suit": "spade",
+    "imgUrl": ""
   },
   "2": {
     "value": 4,
     "name": "4",
     "suit": "spade",
+    "imgUrl": ""
   },
   "3": {
     "value": 5,
     "name": "5",
     "suit": "spade",
+    "imgUrl": ""
   },
   "4": {
     "value": 6,
     "name": "6",
     "suit": "spade",
+    "imgUrl": ""
   },
   "5": {
     "value": 7,
     "name": "7",
     "suit": "spade",
+    "imgUrl": ""
   },
   "6": {
     "value": 8,
     "name": "8",
     "suit": "spade",
+    "imgUrl": ""
   },
   "7": {
     "value": 9,
     "name": "9",
     "suit": "spade",
+    "imgUrl": ""
   },
   "8": {
     "value": 10,
     "name": "10",
     "suit": "spade",
+    "imgUrl": ""
   },
   "9": {
     "value": 10,
     "name": "J",
     "suit": "spade",
+    "imgUrl": ""
   },
   "10": {
     "value": 10,
     "name": "Q",
     "suit": "spade",
+    "imgUrl": ""
   },
   "11": {
     "value": 10,
     "name": "K",
     "suit": "spade",
+    "imgUrl": ""
   },
   "12": {
     "value": 11,
     "name": "A",
     "suit": "spade",
+    "imgUrl": ""
   },
   "13": {
     "value": 2,
     "name": "2",
     "suit": "clubs",
+    "imgUrl": ""
   },
   "14": {
     "value": 3,
     "name": "3",
     "suit": "clubs",
+    "imgUrl": ""
   },
   "15": {
     "value": 4,
     "name": "4",
     "suit": "clubs",
+    "imgUrl": ""
   },
   "16": {
     "value": 5,
     "name": "5",
     "suit": "clubs",
+    "imgUrl": ""
   },
   "17": {
     "value": 6,
     "name": "6",
     "suit": "clubs",
+    "imgUrl": ""
   },
   "18": {
     "value": 7,
     "name": "7",
     "suit": "clubs",
+    "imgUrl": ""
   },
   "19": {
     "value": 8,
     "name": "8",
     "suit": "clubs",
+    "imgUrl": ""
   },
   "20": {
     "value": 9,
     "name": "9",
     "suit": "clubs",
+    "imgUrl": ""
   },
   "21": {
     "value": 10,
     "name": "10",
     "suit": "clubs",
+    "imgUrl": ""
   },
   "22": {
     "value": 10,
     "name": "J",
     "suit": "clubs",
+    "imgUrl": ""
   },
   "23": {
     "value": 10,
     "name": "Q",
     "suit": "clubs",
+    "imgUrl": ""
   },
   "24": {
     "value": 10,
     "name": "K",
     "suit": "clubs",
+    "imgUrl": ""
   },
   "25": {
     "value": 11,
     "name": "A",
     "suit": "clubs",
+    "imgUrl": ""
   },
   "26": {
     "value": 2,
     "name": "2",
     "suit": "diamonds",
+    "imgUrl": ""
   },
   "27": {
     "value": 3,
     "name": "3",
     "suit": "diamonds",
+    "imgUrl": ""
   },
   "28": {
     "value": 4,
     "name": "4",
     "suit": "diamonds",
+    "imgUrl": ""
   },
   "29": {
     "value": 5,
     "name": "5",
     "suit": "diamonds",
+    "imgUrl": ""
   },
   "30": {
     "value": 6,
     "name": "6",
     "suit": "diamonds",
+    "imgUrl": ""
   },
   "31": {
     "value": 7,
     "name": "7",
     "suit": "diamonds",
+    "imgUrl": ""
   },
   "32": {
     "value": 8,
     "name": "8",
     "suit": "diamonds",
+    "imgUrl": ""
   },
   "33": {
     "value": 9,
     "name": "9",
     "suit": "diamonds",
+    "imgUrl": ""
   },
   "34": {
     "value": 10,
     "name": "10",
     "suit": "diamonds",
+    "imgUrl": ""
   },
   "35": {
     "value": 10,
     "name": "J",
     "suit": "diamonds",
+    "imgUrl": ""
   },
   "36": {
     "value": 10,
     "name": "Q",
     "suit": "diamonds",
+    "imgUrl": ""
   },
   "37": {
     "value": 10,
     "name": "K",
     "suit": "diamonds",
+    "imgUrl": ""
   },
   "38": {
     "value": 11,
     "name": "A",
     "suit": "diamonds",
+    "imgUrl": ""
   },
   "39": {
     "value": 2,
     "name": "2",
     "suit": "hearts",
+    "imgUrl": ""
   },
   "40": {
     "value": 3,
     "name": "3",
     "suit": "hearts",
+    "imgUrl": ""
   },
   "41": {
     "value": 4,
     "name": "4",
     "suit": "hearts",
+    "imgUrl": ""
   },
   "42": {
     "value": 5,
     "name": "5",
     "suit": "hearts",
+    "imgUrl": ""
   },
   "43": {
     "value": 6,
     "name": "6",
     "suit": "hearts",
+    "imgUrl": ""
   },
   "44": {
     "value": 7,
     "name": "7",
     "suit": "hearts",
+    "imgUrl": ""
   },
   "45": {
     "value": 8,
     "name": "8",
     "suit": "hearts",
+    "imgUrl": ""
   },
   "46": {
     "value": 9,
     "name": "9",
     "suit": "hearts",
+    "imgUrl": ""
   },
   "47": {
     "value": 10,
     "name": "10",
     "suit": "hearts",
+    "imgUrl": ""
   },
   "48": {
     "value": 10,
     "name": "J",
     "suit": "hearts",
+    "imgUrl": ""
   },
   "49": {
     "value": 10,
     "name": "Q",
     "suit": "hearts",
+    "imgUrl": ""
   },
   "50": {
     "value": 10,
     "name": "K",
     "suit": "hearts",
+    "imgUrl": ""
   },
   "51": {
     "value": 11,
     "name": "A",
     "suit": "hearts",
+    "imgUrl": ""
   },
 };
 console.log(newDeck["35"].name, "of", newDeck["35"].suit);
