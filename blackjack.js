f9a13851f1753b0ed64268348d5ad3b51ee104b9
Alexander Marino <marinoalexp@gmail.com>
2015-07-16 13:29:45 -0400

    cards are in the newDecks

--- blackjack.js
+++ blackjack.js
@@ -7,313 +7,313 @@ var newDeck ={
     "value": 2,
     "name": "2",
     "suit": "spades",
-    "imgUrl": ""
+    "imgUrl": "cards/2_of_spades.png"
   },
   "1": {
     "value": 3,
     "name": "3",
     "suit": "spades",
-    "imgUrl": ""
+    "imgUrl": "cards/3_of_spades.png"
   },
   "2": {
     "value": 4,
     "name": "4",
     "suit": "spades",
-    "imgUrl": ""
+    "imgUrl": "cards/4_of_spades.png"
   },
   "3": {
     "value": 5,
     "name": "5",
     "suit": "spades",
-    "imgUrl": ""
+    "imgUrl": "cards/5_of_spades.png"
   },
   "4": {
     "value": 6,
     "name": "6",
     "suit": "spades",
-    "imgUrl": ""
+    "imgUrl": "cards/6_of_spades.png"
   },
   "5": {
     "value": 7,
     "name": "7",
     "suit": "spades",
-    "imgUrl": ""
+    "imgUrl": "cards/7_of_spades.png"
   },
   "6": {
     "value": 8,
     "name": "8",
     "suit": "spades",
-    "imgUrl": ""
+    "imgUrl": "cards/8_of_spades.png"
   },
   "7": {
     "value": 9,
     "name": "9",
     "suit": "spades",
-    "imgUrl": ""
+    "imgUrl": "cards/9_of_spades.png"
   },
   "8": {
     "value": 10,
     "name": "10",
     "suit": "spades",
-    "imgUrl": ""
+    "imgUrl": "cards/10_of_spades.png"
   },
   "9": {
     "value": 10,
     "name": "Jack",
     "suit": "spades",
-    "imgUrl": ""
+    "imgUrl": "cards/jack_of_spades.png"
   },
   "10": {
     "value": 10,
     "name": "Queen",
     "suit": "spades",
-    "imgUrl": ""
+    "imgUrl": "cards/queen_of_spades.png"
   },
   "11": {
     "value": 10,
     "name": "King",
     "suit": "spades",
-    "imgUrl": ""
+    "imgUrl": "cards/king_of_spades.png"
   },
   "12": {
     "value": 11,
     "name": "Ace",
     "suit": "spades",
-    "imgUrl": ""
+    "imgUrl": "cards/ace_of_spades.png"
   },
   "13": {
     "value": 2,
     "name": "2",
     "suit": "clubs",
-    "imgUrl": ""
+    "imgUrl": "cards/2_of_clubs.png"
   },
   "14": {
     "value": 3,
     "name": "3",
     "suit": "clubs",
-    "imgUrl": ""
+    "imgUrl": "cards/3_of_clubs.png"
   },
   "15": {
     "value": 4,
     "name": "4",
     "suit": "clubs",
-    "imgUrl": ""
+    "imgUrl": "cards/4_of_clubs.png"
   },
   "16": {
     "value": 5,
     "name": "5",
     "suit": "clubs",
-    "imgUrl": ""
+    "imgUrl": "cards/5_of_clubs.png"
   },
   "17": {
     "value": 6,
     "name": "6",
     "suit": "clubs",
-    "imgUrl": ""
+    "imgUrl": "cards/6_of_clubs.png"
   },
   "18": {
     "value": 7,
     "name": "7",
     "suit": "clubs",
-    "imgUrl": ""
+    "imgUrl": "cards/7_of_clubs.png"
   },
   "19": {
     "value": 8,
     "name": "8",
     "suit": "clubs",
-    "imgUrl": ""
+    "imgUrl": "cards/8_of_clubs.png"
   },
   "20": {
     "value": 9,
     "name": "9",
     "suit": "clubs",
-    "imgUrl": ""
+    "imgUrl": "cards/9_of_clubs.png"
   },
   "21": {
     "value": 10,
     "name": "10",
     "suit": "clubs",
-    "imgUrl": ""
+    "imgUrl": "cards/10_of_clubs.png"
   },
   "22": {
     "value": 10,
     "name": "Jack",
     "suit": "clubs",
-    "imgUrl": ""
+    "imgUrl": "cards/jack_of_clubs.png"
   },
   "23": {
     "value": 10,
     "name": "Queen",
     "suit": "clubs",
-    "imgUrl": ""
+    "imgUrl": "cards/queen_of_clubs.png"
   },
   "24": {
     "value": 10,
     "name": "King",
     "suit": "clubs",
-    "imgUrl": ""
+    "imgUrl": "cards/king_of_clubs.png"
   },
   "25": {
     "value": 11,
     "name": "Ace",
     "suit": "clubs",
-    "imgUrl": ""
+    "imgUrl": "cards/ace_of_clubs.png"
   },
   "26": {
     "value": 2,
     "name": "2",
     "suit": "diamonds",
-    "imgUrl": ""
+    "imgUrl": "cards/2_of_diamonds.png"
   },
   "27": {
     "value": 3,
     "name": "3",
     "suit": "diamonds",
-    "imgUrl": ""
+    "imgUrl": "cards/3_of_diamonds.png"
   },
   "28": {
     "value": 4,
     "name": "4",
     "suit": "diamonds",
-    "imgUrl": ""
+    "imgUrl": "cards/4_of_diamonds.png"
   },
   "29": {
     "value": 5,
     "name": "5",
     "suit": "diamonds",
-    "imgUrl": ""
+    "imgUrl": "cards/5_of_diamonds.png"
   },
   "30": {
     "value": 6,
     "name": "6",
     "suit": "diamonds",
-    "imgUrl": ""
+    "imgUrl": "cards/6_of_diamonds.png"
   },
   "31": {
     "value": 7,
     "name": "7",
     "suit": "diamonds",
-    "imgUrl": ""
+    "imgUrl": "cards/7_of_diamonds.png"
   },
   "32": {
     "value": 8,
     "name": "8",
     "suit": "diamonds",
-    "imgUrl": ""
+    "imgUrl": "cards/8_of_diamonds.png"
   },
   "33": {
     "value": 9,
     "name": "9",
     "suit": "diamonds",
-    "imgUrl": ""
+    "imgUrl": "cards/9_of_diamonds.png"
   },
   "34": {
     "value": 10,
     "name": "10",
     "suit": "diamonds",
-    "imgUrl": ""
+    "imgUrl": "cards/10_of_diamonds.png"
   },
   "35": {
     "value": 10,
     "name": "Jack",
     "suit": "diamonds",
-    "imgUrl": ""
+    "imgUrl": "cards/jack_of_diamonds.png"
   },
   "36": {
     "value": 10,
     "name": "Queen",
     "suit": "diamonds",
-    "imgUrl": ""
+    "imgUrl": "cards/queen_of_diamonds.png"
   },
   "37": {
     "value": 10,
     "name": "King",
     "suit": "diamonds",
-    "imgUrl": ""
+    "imgUrl": "cards/king_of_diamonds.png"
   },
   "38": {
     "value": 11,
     "name": "Ace",
     "suit": "diamonds",
-    "imgUrl": ""
+    "imgUrl": "cards/ace_of_diamonds.png"
   },
   "39": {
     "value": 2,
     "name": "2",
     "suit": "hearts",
-    "imgUrl": ""
+    "imgUrl": "cards/2_of_hearts.png"
   },
   "40": {
     "value": 3,
     "name": "3",
     "suit": "hearts",
-    "imgUrl": ""
+    "imgUrl": "cards/3_of_hearts.png"
   },
   "41": {
     "value": 4,
     "name": "4",
     "suit": "hearts",
-    "imgUrl": ""
+    "imgUrl": "cards/4_of_hearts.png"
   },
   "42": {
     "value": 5,
     "name": "5",
     "suit": "hearts",
-    "imgUrl": ""
+    "imgUrl": "cards/5_of_hearts.png"
   },
   "43": {
     "value": 6,
     "name": "6",
     "suit": "hearts",
-    "imgUrl": ""
+    "imgUrl": "cards/6_of_hearts.png"
   },
   "44": {
     "value": 7,
     "name": "7",
     "suit": "hearts",
-    "imgUrl": ""
+    "imgUrl": "cards/7_of_hearts.png"
   },
   "45": {
     "value": 8,
     "name": "8",
     "suit": "hearts",
-    "imgUrl": ""
+    "imgUrl": "cards/8_of_hearts.png"
   },
   "46": {
     "value": 9,
     "name": "9",
     "suit": "hearts",
-    "imgUrl": ""
+    "imgUrl": "cards/9_of_hearts.png"
   },
   "47": {
     "value": 10,
     "name": "10",
     "suit": "hearts",
-    "imgUrl": ""
+    "imgUrl": "cards/10_of_hearts.png"
   },
   "48": {
     "value": 10,
     "name": "Jack",
     "suit": "hearts",
-    "imgUrl": ""
+    "imgUrl": "cards/jack_of_hearts.png"
   },
   "49": {
     "value": 10,
     "name": "Queen",
     "suit": "hearts",
-    "imgUrl": ""
+    "imgUrl": "cards/queen_of_hearts.png"
   },
   "50": {
     "value": 10,
     "name": "King",
     "suit": "hearts",
-    "imgUrl": ""
+    "imgUrl": "cards/king_of_hearts.png"
   },
   "51": {
     "value": 11,
     "name": "Ace",
     "suit": "hearts",
-    "imgUrl": ""
+    "imgUrl": "cards/ace_of_hearts.png"
   },
 };
 
